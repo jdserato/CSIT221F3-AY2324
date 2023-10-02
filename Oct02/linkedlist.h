d639bd2e3b1d2619d300fdbce5b8ad8bf04496f8
Jay Vince Serato <jayvince.serato@cit.edu>
2023-10-02 17:48:00 +0800

    Add doubly linked list

---
+++ Oct02/linkedlist.h
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cstdlib>
+#include "list.h"
+#include "node.h"
+using namespace std;
+
+class DoublyLL : public List {
+	node *head, *tail;
+	int size;
+	
+public:
+	void addFirst(int num) {
+		// Create the node
+		node* n = (node*) calloc( 1, sizeof(node) );
+		n->elem = num;
+		// Step 2: Set the n's next to head
+		n->next = head;
+		if (head) {
+			head->prev = n;
+		}
+		// Step 3: Head to n
+		head = n;
+		if (!tail) {
+			tail = n;
+		}
+		// Step 4: Inc size
+		size++;
+	}
+	
+	void addLast(int num) {
+		// STEP 1: Create node
+		node* n = (node*) malloc( sizeof(node) );
+		cout << "n is " << n << endl;
+		n->elem = num;
+		n->next = nullptr;
+		n->prev = tail;
+		// STEP 2: Tail's next = n
+		if (tail) {
+			cout << "Have tail" << endl;
+			tail->next = n;
+		} else {
+			cout << "Have no tail" << endl;
+			head = n;
+		}
+		// STEP 3: Tail = n
+		tail = n;
+		// STEP 4: Increment size
+		size++;
+	}
+		
+	void add(int num) {
+		addFirst(num);
+	}
+	
+	int get(int pos) {
+		node* curr = head;
+		int ctr = 1;
+		while (ctr < pos) {
+			curr = curr->next;
+			ctr++;
+		}
+		return curr->elem;
+	}
+	
+	int remove(int num) {
+		
+	}
+	
+	void removeFirst() {
+		if (size == 0) {
+			return;
+		}
+		head = head->next;
+		if (head) {
+			free(head->prev);
+			head->prev = nullptr;
+		} else {
+			tail = nullptr;
+		}
+		size--;
+	}
+	
+	void removeLast() {
+		if (size == 0) {
+			return;
+		}
+		tail = tail->prev;
+		if (tail) {
+			free(tail->next);
+			tail->next = nullptr;
+		} else {
+			head = nullptr;
+		}
+		size--;
+	}
+	
+	void print() {
+		node* curr = head;
+		cout << "Size: " << size << endl;
+		cout << "FROM HEAD: ";
+		while (curr != nullptr) {
+			cout << curr->elem << " -> ";
+			curr = curr->next;
+		}
+		curr = tail;
+		cout << endl << "FROM TAIL: ";
+		while (curr != nullptr) {
+			cout << curr->elem << " -> ";
+			curr = curr->prev;
+		}
+		cout << endl;
+	}
+	
+};
